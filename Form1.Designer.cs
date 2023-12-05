a6224e73c4a1dc45dfedae63cecc6b75fe99d455
Sourav Ghosh <141576729+SouravGhosh98@users.noreply.github.com>
2023-12-05 13:29:21 +0530

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace curdoperation
+{
+    partial class CURDOPERATION
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CURDOPERATION));
+            this.labelid = new System.Windows.Forms.Label();
+            this.textBoxID = new System.Windows.Forms.TextBox();
+            this.textBoxname = new System.Windows.Forms.TextBox();
+            this.labelname = new System.Windows.Forms.Label();
+            this.textBoxAge = new System.Windows.Forms.TextBox();
+            this.labelAGE = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btninsert = new System.Windows.Forms.Button();
+            this.btnsearch = new System.Windows.Forms.Button();
+            this.btnupdate = new System.Windows.Forms.Button();
+            this.btndelet = new System.Windows.Forms.Button();
+            this.labelresult = new System.Windows.Forms.Label();
+            this.dataGridresult = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridresult)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelid
+            // 
+            this.labelid.AutoSize = true;
+            this.labelid.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.labelid.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelid.Font = new System.Drawing.Font("Microsoft Uighur", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelid.Location = new System.Drawing.Point(71, 35);
+            this.labelid.Name = "labelid";
+            this.labelid.Size = new System.Drawing.Size(36, 27);
+            this.labelid.TabIndex = 0;
+            this.labelid.Text = "ID:-";
+            // 
+            // textBoxID
+            // 
+            this.textBoxID.Location = new System.Drawing.Point(157, 37);
+            this.textBoxID.Multiline = true;
+            this.textBoxID.Name = "textBoxID";
+            this.textBoxID.Size = new System.Drawing.Size(375, 28);
+            this.textBoxID.TabIndex = 1;
+            // 
+            // textBoxname
+            // 
+            this.textBoxname.Font = new System.Drawing.Font("Microsoft Uighur", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxname.Location = new System.Drawing.Point(157, 90);
+            this.textBoxname.Name = "textBoxname";
+            this.textBoxname.Size = new System.Drawing.Size(375, 28);
+            this.textBoxname.TabIndex = 3;
+            // 
+            // labelname
+            // 
+            this.labelname.AutoSize = true;
+            this.labelname.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.labelname.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelname.Font = new System.Drawing.Font("Microsoft Uighur", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelname.Location = new System.Drawing.Point(71, 90);
+            this.labelname.Name = "labelname";
+            this.labelname.Size = new System.Drawing.Size(62, 27);
+            this.labelname.TabIndex = 2;
+            this.labelname.Text = "NAME:-";
+            // 
+            // textBoxAge
+            // 
+            this.textBoxAge.Location = new System.Drawing.Point(157, 140);
+            this.textBoxAge.Multiline = true;
+            this.textBoxAge.Name = "textBoxAge";
+            this.textBoxAge.Size = new System.Drawing.Size(375, 28);
+            this.textBoxAge.TabIndex = 5;
+            // 
+            // labelAGE
+            // 
+            this.labelAGE.AutoSize = true;
+            this.labelAGE.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.labelAGE.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelAGE.Font = new System.Drawing.Font("Microsoft Uighur", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelAGE.Location = new System.Drawing.Point(71, 143);
+            this.labelAGE.Name = "labelAGE";
+            this.labelAGE.Size = new System.Drawing.Size(49, 27);
+            this.labelAGE.TabIndex = 4;
+            this.labelAGE.Text = "AGE:-";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Purple;
+            this.pictureBox1.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.ErrorImage")));
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(538, 37);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(326, 137);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btninsert
+            // 
+            this.btninsert.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btninsert.Font = new System.Drawing.Font("Microsoft Uighur", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btninsert.Location = new System.Drawing.Point(67, 196);
+            this.btninsert.Name = "btninsert";
+            this.btninsert.Size = new System.Drawing.Size(75, 31);
+            this.btninsert.TabIndex = 8;
+            this.btninsert.Text = "INSERT";
+            this.btninsert.UseVisualStyleBackColor = false;
+            this.btninsert.Click += new System.EventHandler(this.btninsert_Click);
+            // 
+            // btnsearch
+            // 
+            this.btnsearch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnsearch.Font = new System.Drawing.Font("Microsoft Uighur", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsearch.Location = new System.Drawing.Point(437, 196);
+            this.btnsearch.Name = "btnsearch";
+            this.btnsearch.Size = new System.Drawing.Size(95, 31);
+            this.btnsearch.TabIndex = 9;
+            this.btnsearch.Text = "SEARCH";
+            this.btnsearch.UseVisualStyleBackColor = false;
+            this.btnsearch.Click += new System.EventHandler(this.btnsearch_Click);
+            // 
+            // btnupdate
+            // 
+            this.btnupdate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnupdate.Font = new System.Drawing.Font("Microsoft Uighur", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnupdate.Location = new System.Drawing.Point(313, 196);
+            this.btnupdate.Name = "btnupdate";
+            this.btnupdate.Size = new System.Drawing.Size(82, 31);
+            this.btnupdate.TabIndex = 10;
+            this.btnupdate.Text = "UPDATE";
+            this.btnupdate.UseVisualStyleBackColor = false;
+            this.btnupdate.Click += new System.EventHandler(this.btnupdate_Click);
+            // 
+            // btndelet
+            // 
+            this.btndelet.BackColor = System.Drawing.Color.Red;
+            this.btndelet.Font = new System.Drawing.Font("Microsoft Uighur", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btndelet.Location = new System.Drawing.Point(183, 196);
+            this.btndelet.Name = "btndelet";
+            this.btndelet.Size = new System.Drawing.Size(83, 31);
+            this.btndelet.TabIndex = 11;
+            this.btndelet.Text = "DELETE";
+            this.btndelet.UseVisualStyleBackColor = false;
+            this.btndelet.Click += new System.EventHandler(this.btndelet_Click);
+            // 
+            // labelresult
+            // 
+            this.labelresult.AutoSize = true;
+            this.labelresult.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.labelresult.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelresult.Location = new System.Drawing.Point(18, 310);
+            this.labelresult.Name = "labelresult";
+            this.labelresult.Size = new System.Drawing.Size(115, 26);
+            this.labelresult.TabIndex = 12;
+            this.labelresult.Text = "# RESULT:-";
+            // 
+            // dataGridresult
+            // 
+            this.dataGridresult.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.dataGridresult.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridresult.Location = new System.Drawing.Point(210, 233);
+            this.dataGridresult.Name = "dataGridresult";
+            this.dataGridresult.Size = new System.Drawing.Size(340, 200);
+            this.dataGridresult.TabIndex = 13;
+            // 
+            // CURDOPERATION
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.GreenYellow;
+            this.ClientSize = new System.Drawing.Size(862, 450);
+            this.Controls.Add(this.dataGridresult);
+            this.Controls.Add(this.labelresult);
+            this.Controls.Add(this.btndelet);
+            this.Controls.Add(this.btnupdate);
+            this.Controls.Add(this.btnsearch);
+            this.Controls.Add(this.btninsert);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBoxAge);
+            this.Controls.Add(this.labelAGE);
+            this.Controls.Add(this.textBoxname);
+            this.Controls.Add(this.labelname);
+            this.Controls.Add(this.textBoxID);
+            this.Controls.Add(this.labelid);
+            this.Name = "CURDOPERATION";
+            this.Text = "CURD OPERATION USING C# AND  .NET(WINDOWS FROM)";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridresult)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelid;
+        private System.Windows.Forms.TextBox textBoxID;
+        private System.Windows.Forms.TextBox textBoxname;
+        private System.Windows.Forms.Label labelname;
+        private System.Windows.Forms.TextBox textBoxAge;
+        private System.Windows.Forms.Label labelAGE;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btninsert;
+        private System.Windows.Forms.Button btnsearch;
+        private System.Windows.Forms.Button btnupdate;
+        private System.Windows.Forms.Button btndelet;
+        private System.Windows.Forms.Label labelresult;
+        private System.Windows.Forms.DataGridView dataGridresult;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace curdoperation
+{
+    public partial class CURDOPERATION : Form
+    {
+        private SqlConnection connection;
+        string connectionstring= "Data Source=DESKTOP-EQOL92T\\SQLEXPRESS;Initial Catalog=CURD;Integrated Security=True";
+        bool s=false;
+        public CURDOPERATION()
+        {
+            InitializeComponent();
+            try
+            { 
+                connection = new SqlConnection(connectionstring);  //DESKTOP-EQOL92T\SQLEXPRESS
+                connection.Open();
+                dataGridresult.CellClick += dataGridresult_CellClick;
+                loaddata();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error connecting to the Database:"+ex.Message);    
+            }
+        }
+        private void btninsert_Click(object sender, EventArgs e)
+        {
+            string insertQuery = "INSERT INTO Companies (id,name,AGE) VALUES (@id,@name,@AGE)";
+            using (SqlCommand comd = new SqlCommand(insertQuery, connection))
+            {
+                comd.Parameters.AddWithValue("@id", textBoxID.Text);
+                comd.Parameters.AddWithValue("@name", textBoxname.Text);
+                comd.Parameters.AddWithValue("@AGE", textBoxAge.Text);
+                s=ValidateInput();
+                if(s==true)
+                {
+                   int rowsAffected = comd.ExecuteNonQuery();
+                     if (rowsAffected > 0)
+                     { 
+                        MessageBox.Show("Data inserted successfully.");
+                        s=false;
+                     }
+                    else
+                    { 
+                       MessageBox.Show("Failed to insert data.");
+                    }
+                  loaddata();
+                }
+            }
+        }
+        private void btndelet_Click(object sender, EventArgs e)
+        {
+            string deleteQuery = "DELETE FROM Companies WHERE ID = @id";
+            using (SqlCommand comd = new SqlCommand(deleteQuery, connection))
+            {
+                comd.Parameters.AddWithValue("@id", textBoxID.Text);
+                int rowsAffected = comd.ExecuteNonQuery();
+                if (rowsAffected > 0)
+                { 
+                    MessageBox.Show("Data deleted successfully.");
+                    textBoxID.Text=" ";
+                    textBoxname.Text=" ";
+                    textBoxAge.Text=" ";
+                }
+                else
+                { 
+                    MessageBox.Show("Failed to delete data.");
+                    textBoxID.Text=" ";
+                    textBoxname.Text=" ";
+                    textBoxAge.Text=" ";
+                }
+            }
+            loaddata();
+        }
+        private void btnupdate_Click(object sender, EventArgs e)
+        {
+            bool k=false;
+            string updateQuery = "UPDATE Companies SET name = @name, AGE = @AGE WHERE id = @ID";
+             s=ValidateInput();
+             if (s==true)
+             {
+                using (SqlCommand comd = new SqlCommand(updateQuery, connection))
+                {
+                   comd.Parameters.AddWithValue("@name", textBoxname.Text);
+                   comd.Parameters.AddWithValue("@AGE", textBoxAge.Text);
+                   comd.Parameters.AddWithValue("@id", textBoxID.Text);
+                    k=ValidateInput();
+                    {
+                        if(k==true)
+                        {
+                         int rowsAffected = comd.ExecuteNonQuery();
+                          if (rowsAffected > 0)
+                          { 
+                             MessageBox.Show("Data updated successfully.");
+                          }
+                         else
+                         { 
+                             MessageBox.Show("Failed to update data.");
+                         }
+                        }
+                    }
+                }
+                loaddata();
+             }
+        }
+        private void btnsearch_Click(object sender, EventArgs e)
+        {
+            string searchQuery = "SELECT * FROM Companies WHERE id = @id";
+                using (SqlCommand comd = new SqlCommand(searchQuery, connection))
+                {
+                  comd.Parameters.AddWithValue("@id", textBoxID.Text);
+                   using (SqlDataReader reader = comd.ExecuteReader())
+                   {
+                     if (reader.Read())
+                     {
+                        int id = Convert.ToInt32(reader["id"]);
+                        string name = reader["name"].ToString();
+                        int age = Convert.ToInt32(reader["AGE"]);
+                        MessageBox.Show($"ID: {id}\n Name: {name}\n Age: {age}");
+                     }
+                     else
+                     {
+                        textBoxID.Text=" ";
+                        textBoxname.Text=" ";
+                        textBoxAge.Text=" ";
+                        MessageBox.Show("Data not found.");
+                     }
+                   }
+                }
+               loaddata();
+        }
+        private void loaddata()
+        {
+                string selectQuery = "SELECT * FROM Companies";
+                using (SqlDataAdapter adapter = new SqlDataAdapter(selectQuery, connection))
+                {
+                    DataTable dataTable = new DataTable();
+                    adapter.Fill(dataTable);
+                    dataGridresult.DataSource = dataTable;
+                }
+        }
+        private bool ValidateInput()
+        {
+               if (string.IsNullOrWhiteSpace(textBoxname.Text))
+               {
+                   MessageBox.Show("Please enter a name.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                   textBoxname.Text="\0";
+                   return false;
+               }
+               foreach (char character in textBoxname.Text)
+               {
+                  if (!char.IsLetter(character))
+                  {
+                      MessageBox.Show("Name should contain only letters.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                      textBoxname.Text="\0";
+                      return false;
+                  }
+               }
+               if (!int.TryParse(textBoxID.Text, out _))
+               {
+                  MessageBox.Show("Please enter a valid ID.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                  textBoxID.Text="\0";
+                  return false;
+               }
+               if (!int.TryParse(textBoxAge.Text, out _))
+               {
+                  MessageBox.Show("Please enter a valid age.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                  textBoxAge.Text="\0";
+                  return false;
+               }  
+              return true;
+        }
+        private void dataGridresult_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0) 
+            {
+               DataGridViewRow selectedRow = dataGridresult.Rows[e.RowIndex];
+               textBoxID.Text = selectedRow.Cells["id"].Value.ToString();
+               textBoxname.Text = selectedRow.Cells["name"].Value.ToString();
+               textBoxAge.Text = selectedRow.Cells["AGE"].Value.ToString();
+            }
+        }
+
+    }
+}
